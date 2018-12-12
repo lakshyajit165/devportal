80cfed0bc85d4bf7fc04894e0d087d62250bdf86
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-12 19:31:20 +0530

    Lecture 13 complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 //Load User Model
 const User = require('../../models/User');
@@ -94,6 +95,17 @@ router.post('/login', (req,res) => {
                       }
                   })
         });
+});
+
+// @route  GET api/users/current
+// @desc   Return current user
+// @access private
+router.get('/current', passport.authenticate('jwt', {session: false}), (req,res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
 });
 
 module.exports = router;
