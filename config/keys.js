22817c62df33e133423b09b9c19ee4869b8b0cd2
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-12 14:50:47 +0530

    Lecture 12 complete

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://lakshyajit:gagool123@ds131954.mlab.com:31954/devportal'
+    mongoURI: 'mongodb://lakshyajit:gagool123@ds131954.mlab.com:31954/devportal',
+    secretOrKey: 'secret'
 };
