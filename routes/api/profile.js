a230dcd3c2c0dd8bede6929b8c34ca339eaae6a0
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-14 22:50:31 +0530

    Lecture 19, Create and update profile routes complete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Profile Model
+const Profile = require('../../models/Profile');
+
+// Load User Model
+const User = require('../../models/User');
 
 
 // @route  GET api/profile/test
@@ -8,4 +16,19 @@ const router = express.Router();
 
 router.get('/test', (req,res) => res.json({msg: "Profile Works"}));
 
+// @route  GET api/profile
+// @desc   Get current user's profile
+// @access private
+router.get('/', passport.authenticate('jwt', { session: false}), (req,res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+        if(!profile){
+            errors.noprofile = 'There is no profile for this user!';
+            return res.status(404).json(errors);
+        }    
+        res.json(profile);    
+    })
+    .catch(err => res.status(404).json(err));
+});
 module.exports = router;
