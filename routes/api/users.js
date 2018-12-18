b09d11d149980e3a130f3fb0f5a3192aab0929b1
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-18 15:46:27 +0530

    Section 5 Post API Routes complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,7 +51,7 @@ router.post('/register', (req,res) => {
             const newUser = new User({
                 name: req.body.name,
                 email: req.body.email,
-                avatar,
+                avatar, //avatar: avatar
                 password: req.body.password
             });
 
--- validation/login.js
+++ validation/login.js
@@ -8,15 +8,15 @@ module.exports = function validateLoginInput(data) {
   data.password = !isEmpty(data.password) ? data.password : '';
 
   if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
+    errors.email = 'Email is invalid!';
   }
 
   if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
+    errors.email = 'Email field is required!';
   }
 
   if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password field is required';
+    errors.password = 'Password field is required!';
   }
 
   return {
