014f5f634b1ce2168b5871ed8cb22812d17ac6bf
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-20 12:20:40 +0530

    Section 6 complete!

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path = "/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
