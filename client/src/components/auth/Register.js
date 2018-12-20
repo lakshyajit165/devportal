ce939d1ee53b60267e4ec6adab743c4352af7d3a
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-20 15:34:01 +0530

    Section 7 complete

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import { timingSafeEqual } from 'crypto';
+import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
   
@@ -32,10 +33,14 @@ class Register extends Component {
             password: this.state.password,
             password2: this.state.password2
         }
-        console.log(newUser);
+        axios.post('api/users/register', newUser)
+             .then(res => console.log(res.data))
+             .catch(err => this.setState({errors: err.response.data}));
     }
 
     render() {
+
+    const { errors } = this.state;    
     return (
       <div className="register">
             <div className="container">
@@ -45,20 +50,38 @@ class Register extends Component {
                 <p className="lead text-center">Create your DevConnector account</p>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
-                    <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} />
+                        <input type="text" className={classnames('form-control form-control-lg', { 'is-invalid': errors.name })} placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} />
+                        {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
                     </div>
+
                     <div className="form-group">
-                    <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange}/>
-                    <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                        <input type="email" className={classnames('form-control form-control-lg', { 'is-invalid': errors.email })} placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange}/>
+                        
+                        {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
+                        
+                        <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                     </div>
+
                     <div className="form-group">
-                    <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
+                        
+                        <input type="password" className={classnames('form-control form-control-lg', { 'is-invalid': errors.password })} placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
+
+                        {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
+
                     </div>
+
                     <div className="form-group">
-                    <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange}/>
+
+                        <input type="password" className={classnames('form-control form-control-lg', { 'is-invalid': errors.password2 })} placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange}/>
+
+                        {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
+
                     </div>
+
                     <div className="text-center">
-                    <input type="submit" className="btn btn-info btn-block mt-4" />
+
+                        <input type="submit" className="btn btn-info btn-block mt-4" />
+                    
                     </div>  
                 </form>
                 </div>
