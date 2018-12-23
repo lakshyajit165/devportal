60b2f3fd73616523a5373c6dcd905793b1c84966
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-23 19:45:29 +0530

    Section 9 lecture 46 complete

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
 import { loginUser } from '../../actions/authActions'; 
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Login extends Component {
 
@@ -64,28 +65,26 @@ class Login extends Component {
                 <h1 className="display-4 text-center">Log In</h1>
                 <p className="lead text-center">Sign in to your DevConnector account</p>
                 <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                    <input 
-                        type="email" 
-                        className={classnames('form-control form-control-lg', { 'is-invalid': errors.email })} 
-                        placeholder="Email Address" 
-                        name="email" 
-                        value={this.state.email} 
+
+                    <TextFieldGroup 
+                        placeholder="Email Address"
+                        name="email"
+                        type="email"
+                        value={this.state.email}
                         onChange={this.onChange}
+                        error={errors.email}
                     />
-                    {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
-                    </div>
-                    <div className="form-group">
-                    <input 
-                        type="password" 
-                        className={classnames('form-control form-control-lg', { 'is-invalid': errors.password })} 
-                        placeholder="Password" 
-                        name="password" 
-                        value={this.state.password} 
+
+                    <TextFieldGroup 
+                        placeholder="Password"
+                        name="password"
+                        type="password"
+                        value={this.state.password}
                         onChange={this.onChange}
+                        error={errors.password}
                     />
-                    {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
-                    </div>
+                    
+                    
                     <div className="text-center">
                     <input type="submit" className="btn btn-info btn-block mt-4" />
                     </div>  
