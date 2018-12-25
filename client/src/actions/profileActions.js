f0aa682b4104a1b87a35e01f6a730bb41c117fb1
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-26 02:06:03 +0530

    Section 9 complete

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -21,6 +21,19 @@ export const getCurrentProfile = () => dispatch => {
          );
 }
 
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios
+        .post('/api/profile', profileData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
+
 // Profile Loading
 export const setProfileLoading = () => {
     return {
