046bb8de4f51b31dc78c38244716327636fd786b
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-25 23:25:08 +0530

    Section 9 Lecture 49 complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
@@ -52,7 +54,10 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={ Register }/>
               <Route exact path="/login" component={ Login }/>
-              <Route exact path="/dashboard" component={ Dashboard }/>
+              <Switch>
+                  <PrivateRoute exact path="/dashboard" component={ Dashboard }/>
+              </Switch>
+              
             </div>
             <Footer />
           </div>
