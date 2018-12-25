ddf9139648d4d344940c7d510942f865f9c58bb8
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-26 01:32:05 +0530

    Section 9 Lecture 52 complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -16,7 +17,9 @@ import './App.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
-import { clearCurrentProfile } from './actions/profileActions';
+import CreateProfile from './components/create-profile/CreateProfile';
+
+
 
 // Check for token
 if(localStorage.jwtToken){
@@ -56,6 +59,9 @@ class App extends Component {
               <Route exact path="/login" component={ Login }/>
               <Switch>
                   <PrivateRoute exact path="/dashboard" component={ Dashboard }/>
+              </Switch>
+              <Switch>
+                  <PrivateRoute exact path="/create-profile" component={ CreateProfile }/>
               </Switch>
               
             </div>
