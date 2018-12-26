21bef2cf1b27976b7470d34f077a671137f11f06
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-27 02:34:17 +0530

    Section 10 lecture 58

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -34,6 +34,34 @@ export const createProfile = (profileData, history) => dispatch => {
         );
 }
 
+// Add Experience
+
+export const addExperience = (expData, history) => dispatch => {
+    axios
+        .post('/api/profile/experience', expData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
+
+// Add Education
+
+export const addEducation = (eduData, history) => dispatch => {
+    axios
+        .post('/api/profile/education', eduData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
+
 // Delete Account & Profile
 export const deleteAccount = () => dispatch => {
     if(window.confirm('Are you sure? This can\'t be undone!')){
