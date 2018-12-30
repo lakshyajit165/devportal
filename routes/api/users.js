24ebafa768d28cc53822a426d9ef5ac165851210
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-31 02:01:09 +0530

    Added Mailing Functionality on Sign up

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+//email
+const nodemailer = require('nodemailer');
+
 // Load input validation
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
@@ -64,6 +67,55 @@ router.post('/register', (req,res) => {
                             .catch(err => console.log(err));  
                 })
             })
+
+            // Send Email
+
+            const output = `
+                <div style="font-family: Calibri;">
+                <h2>Hello ${req.body.name}!!</h2>
+                <h3>Thanks for Signing Up at Devportal!</h3>
+                <hr>
+                
+                <h3>Stay tuned to this site to connect and developers...Happy Learning!...</h3></div>`;
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com', //'smtp.ethereal.email',
+            port: 587,//587
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: 'lakshmeekant@gmail.com', // generated ethereal user
+                pass: 'lcxlzbdxwsvdcwno'//account.pass ''// generated ethereal password
+            },
+            tls:{
+                rejectUnauthorized: false
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: 'DevPortal', // sender address
+            to: `${req.body.email}`, // list of receivers
+            subject: 'DevPortal Signup', // Subject line
+            text: 'Hello world?', // plain text body
+            html: output // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+          
+
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        });
 
         }
     })
