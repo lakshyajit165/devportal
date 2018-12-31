29cf64057ef5ea4ba636f5c99b48b6992c7fd55c
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-31 20:17:14 +0530

    Prepared for deployment

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://lakshyajit:gagool123@ds131954.mlab.com:31954/devportal',
-    secretOrKey: 'secret'
-};
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://lakshyajit:gagool123@ds131954.mlab.com:31954/devportal',
+    secretOrKey: 'secret'
+};
